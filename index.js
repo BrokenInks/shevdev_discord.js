bab10bc4afef683abcb1b85fb63caae1c99d2145
Kirill Beliy <75167018+beliykirill@users.noreply.github.com>
2021-03-01 10:56:55 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const DiscordJS = require('discord.js')
+const WOKCommands = require('wokcommands')
+require('dotenv').config()
+
+const client = new DiscordJS.Client({
+  partials: ['MESSAGE', 'REACTION'],
+})
+
+client.on('ready', () => {
+    const umnie = 'Бот готов!'
+    console.log(umnie)
+  const dbOptions = {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    }
+
+  
+  new WOKCommands(client, {
+    commandsDir: 'commands',
+    featureDir: 'features',
+    dbOptions
+  })
+    .setDefaultPrefix('.')
+})
+
+client.login(process.env.TOKEN)
