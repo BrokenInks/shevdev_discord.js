0a957734b1abd57e275fb99e8a55580b6c35a611
Kirill Beliy <75167018+beliykirill@users.noreply.github.com>
2021-05-10 15:42:20 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const DiscordJS = require('discord.js')
+const WOKCommands = require('wokcommands')
+require('dotenv').config()
+const mongoCurrency = require('discord-mongo-currency');
+ 
+mongoCurrency.connect('mongodb://localhost:27017/tutorial');
+const discordTTS = require('discord-tts')
+
+const client = new DiscordJS.Client({
+  partials: ['MESSAGE', 'REACTION'],
+})
+
+client.on('ready', () => {
+  console.log('Бот готов к работе!')
+  const dbOptions = {
+    keepAlive: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    }
+  new WOKCommands(client, {
+    commandsDir: 'commands',
+    featureDir: 'features',
+    dbOptions
+  })
+    .setMongoPath(process.env.MONGO_URI)
+    .setDefaultPrefix('.')
+})
+
+
+client.login(process.env.TOKEN)
